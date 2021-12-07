bb9e23f7b4a857ea1f47524f55d35afeb17b4cc2
PreviousLife <thanhthientran1511@gmail.com>
2021-12-07 17:06:00 +0700

    push project mern tutorial

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,1 +1,1 @@
-export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://192.168.1.7:5000/api' : 'http'
+export const apiUrl = process.env.NODE_ENV !== 'production' ? 'http://192.168.1.7:5000/api' : 'https://dry-beach-88174.herokuapp.com/api'
